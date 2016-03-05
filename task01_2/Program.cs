8416ca0232a8bc6eff3285aa1612fd49836ac89d
Daria Kuzina <missdashak@gmail.com>
2016-03-05 17:30:07 +0300

    Problem "replace only one max with only one min" fixed

--- task01_2/Program.cs
+++ task01_2/Program.cs
@@ -18,6 +18,12 @@ namespace Homework2_2
             public int value;
             public int i;
             public int j;
+            public Item(int value, int i, int j)
+            {
+                this.value = value;
+                this.i = i;
+                this.j = j;
+            }
             public int CompareTo(Object item)
             {
                 return CompareTo((Item)item);
@@ -25,6 +31,15 @@ namespace Homework2_2
             public int CompareTo(Item item)
             {
                 return value.CompareTo(item.value);
+            }
+            public override bool Equals(Object item)
+            {
+                    return value.Equals(((Item)item).value);
+            }
+
+            public override int GetHashCode()
+            {
+                return this.value.GetHashCode();
             }
 
         }
@@ -64,6 +79,27 @@ namespace Homework2_2
 
         }
         /// <summary>
+        /// На место элемента item1 ставит item2, и наоборот
+        /// </summary>
+        /// <param name="arr">Двумерный массив</param>
+        /// <param name="item1">Первый элемент</param>
+        /// <param name="item2">Второй элемент</param>
+        public static void SwapElements(int[,] arr, int item1, int item2)
+        {
+            int n = arr.GetLength(0);
+            int m = arr.GetLength(1);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (arr[i, j] == item1)
+                        arr[i, j] = item2;
+                    else if (arr[i, j] == item2)
+                        arr[i, j] = item1;
+                }
+            }
+        }
+        /// <summary>
         /// Находит минимальный элемент в двумерном массиве
         /// </summary>
         /// <param name="arr">Двумерный массив</param>
@@ -99,8 +135,10 @@ namespace Homework2_2
             int m = arr.GetLength(1);
             int min = arr[0, 0];
             int max = arr[0, 0];
-            int minIndex = 0;
-            int maxIndex = 0;
+            int minIndexX = 0;
+            int minIndexY = 0;
+            int maxIndexX = 0;
+            int maxIndexY = 0;
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
@@ -108,40 +146,40 @@ namespace Homework2_2
                     if (arr[i, j] < min)
                     {
                         min = arr[i, j];
-                        minIndex = i * m + j;
+                        minIndexX = i;
+                        minIndexY = j;
                     }
                     else if (arr[i, j] > max)
                     {
                         max = arr[i, j];
-                        maxIndex = i * m + j;
+                        maxIndexX = i;
+                        maxIndexY = j;
                     }
                 }
             }
-            arr[maxIndex / m, maxIndex % m] = min;
-            arr[minIndex / m, minIndex % m] = max;
-
+            SwapElements(arr, arr[maxIndexX, maxIndexY], arr[minIndexX, minIndexY]);
         }
         /// <summary>
-        /// Создает отсортированный массив структур  вида {элемент, индекс в исходном массиве}
+        /// Создает отсортированный список структур вида {элемент, индекс в исходном массиве} без повторений
         /// </summary>
         /// <param name="arr">Двумерный массив</param>
-        /// <returns>Одномерный отсортированный массив</returns>
-        private static Item[] CreateSortedArray(int[,] arr)
+        /// <returns>Одномерный отсортированный список без повторений</returns>
+        private static List<Item> CreateSortedArray(int[,] arr)
         {
             int n = arr.GetLength(0);
             int m = arr.GetLength(1);
-            Item[] sortedArray = new Item[n * m];
+            List<Item> sortedArray = new List<Item>();
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
                 {
                     int index = i * m + j;
-                    sortedArray[index].value = arr[i, j];
-                    sortedArray[index].i = i;
-                    sortedArray[index].j = j;
+                    Item item = new Item(arr[i, j], i, j);
+                    if (sortedArray.IndexOf(item) < 0)
+                        sortedArray.Add(item);
                 }
             }
-            Array.Sort(sortedArray);
+            sortedArray.Sort();
             return sortedArray;
         }
         /// <summary>
@@ -154,18 +192,15 @@ namespace Homework2_2
             if (k >= arr.GetLength(0) * arr.GetLength(1))
                 throw new IndexOutOfRangeException();
 
-            Item[] sortedArray = CreateSortedArray(arr);
-            int m = arr.GetLength(1);
-            int last = m * arr.GetLength(0) - 1;
-
-            for (int i = 0; i < k; i++)
+            List<Item> sortedArray = CreateSortedArray(arr);
+            int last = sortedArray.Count-1;
+            for (int i = 0; i < k&&i<last; i++)
             {
                 int minX = sortedArray[i].i;
                 int minY = sortedArray[i].j;
                 int maxX = sortedArray[last - i].i;
                 int maxY = sortedArray[last - i].j;
-                arr[minX, minY] = sortedArray[last - i].value;
-                arr[maxX, maxY] = sortedArray[i].value;
+                SwapElements(arr, arr[maxX, maxY], arr[minX, minY]);
             }
 
         }
@@ -176,7 +211,7 @@ namespace Homework2_2
         static void Main(string[] args)
         {
             int n = 3;
-            int m = 5;
+            int m = 3;
             int[,] numbers = new int[n, m];
             Console.WriteLine("Введите {0} целых чисел (по одному в строке)", n * m);
 
@@ -190,8 +225,9 @@ namespace Homework2_2
             SwapMinWithMax(numbers);
             Console.WriteLine("После обмена минимума и максимума:");
             Show(numbers);
+            Console.WriteLine();
 
-            int k = 5;
+            int k = 3;
             try
             {
                 SwapKExtremums(numbers, k);
